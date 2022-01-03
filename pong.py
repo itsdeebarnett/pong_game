d52f632daead360309a5a54b4890042403bbcf61
itsdeebarnett <jdeebarnett@gmail.com>
2022-01-03 00:38:57 -0500

    ball error lol

--- pong.py
+++ pong.py
@@ -31,7 +31,8 @@ ball.shape("square")
 ball.color("red")
 ball.penup()
 ball.goto(0, 0)
-
+ball.dx = .2
+ball.dy = -.2
 
 #function 
 def paddle_a_up():
@@ -65,3 +66,22 @@ window.onkeypress(paddle_b_down, "Down")
 
 while True:
     window.update()
+
+    #ball movement
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    # Border checking
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1 #reverses the direction of the ball
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+
+    #left and right border
+    if ball.xcor()> 390:
+        ball.goto(0)
+        ball.dx *= -1
+
