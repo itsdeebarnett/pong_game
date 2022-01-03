ce751ae51e443c6ccde1095374a98ae66df103f3
itsdeebarnett <jdeebarnett@gmail.com>
2022-01-03 00:17:41 -0500

    pong ball and the ability to move

--- pong.py
+++ pong.py
@@ -25,8 +25,41 @@ paddle_b.penup()
 paddle_b.goto(350, 0)
 
 #Ball
+ball= turtle.Turtle()
+ball.speed(0) #speed of animation 
+ball.shape("square")
+ball.color("red")
+ball.penup()
+ball.goto(0, 0)
 
 
+#function 
+def paddle_a_up():
+    y = paddle_a.ycor() #defines y cordinates 
+    y += 20 # adds 20 pixels to y 
+    paddle_a.sety(y)
+
+def paddle_a_down():
+    y = paddle_a.ycor() 
+    y -= 20 # subtract 20 pixels to y 
+    paddle_a.sety(y)
+
+def paddle_b_up():
+    y = paddle_b.ycor() #defines y cordinates 
+    y += 20 # adds 20 pixels to y 
+    paddle_b.sety(y)
+
+def paddle_b_down():
+    y = paddle_b.ycor() 
+    y -= 20 # subtract 20 pixels to y 
+    paddle_b.sety(y)
+
+#keyboard binding
+window.listen() # This tells it to listen to keyboard input from turtle module
+window.onkeypress(paddle_a_up, "w")
+window.onkeypress(paddle_a_down, "s")
+window.onkeypress(paddle_b_up, "Up")
+window.onkeypress(paddle_b_down, "Down")
 
 # Main game loop
 
